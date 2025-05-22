0b0145afc5d6966fa819a6859c6673efa8eb896a
Sneha-cm <132684433+Sneha-cm@users.noreply.github.com>
2025-05-22 09:15:48 +0530

    Add files via upload

---
+++ java/Hello.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String[] students = new String[10];
+		System.out.println(students.length);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
