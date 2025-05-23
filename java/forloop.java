ce3d0b844850fc626bac2cd49d532c390acae74d
Sneha-cm <132684433+Sneha-cm@users.noreply.github.com>
2025-05-23 09:52:36 +0530

    Add files via upload

---
+++ java/forloop.java
@@ -0,0 +1,11 @@
+package project1;
+
+class forloop {
+    public static void main(String args[]) {
+        // for loop begins when x=0
+        // and runs till x <=10
+        
+        for (int x = 0; x <= 10; x++)
+            System.out.println("Value of x:" + x);
+    }
+}
